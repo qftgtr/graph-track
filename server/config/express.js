a6d44be3091f21799c8c0744ae90c8ea2ee5d836
qftgtr <qft.gtr@gmail.com>
2016-03-20 10:09:55 +0800

    set params

--- server/config/express.js
+++ server/config/express.js
@@ -56,7 +56,7 @@ export default function(app) {
   // We need to enable sessions for passport-twitter because it's an
   // oauth 1.0 strategy, and Lusca depends on sessions
   app.use(/\/((?!api\/track).)*/, session({
-    name: 'trackid',
+    name: 'sid',
     secret: config.secrets.session,
     saveUninitialized: true,
     resave: false,
